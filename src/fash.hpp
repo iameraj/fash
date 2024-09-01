10b801fcc3a823ab701194c85925bcc76759fa85
iameraj <iameraj@outlook.com>
2024-09-01 19:24:57 +0530

    formatting

--- src/fash.hpp
+++ src/fash.hpp
@@ -1,33 +1,36 @@
+#include <string>
+#include <iostream>
 // FaSh - Fast Shell parent class for each session
 class fash {
-   private:
-    // Current path of the session. starts with the parent directory.
-    std::string current_path;
+	private:
+		// Current path of the session. starts with the parent directory.
+		std::string current_path;
 
-    // Every function is a implementation of the respective commands. the
-    // function here should not take any arguments.They have to ask for
-    // necessary arguments themselves. They shall return a exit status as [int]
-    // helper function will be static and flexible according to the needs
-   public:
-    // provide the starting path to the shell
-    fash(std::string path) {
-        this->current_path = path;
-        std::cout << "...initializing fash at - " << path << std::endl;
-        system("pause");
-        system("cls");
-    };
-    int welcome_msg(void);
-    int exit_msg(void);
-    int run_sh(void);
-    int list_dir(void);
-    int change_dir(void);
-    int make_dir(void);
-    int remove_dir(void);
-    int remove_item(void);
-    int copy_item(void);
-    int clear_scr(void);
+		// Every function is a implementation of the respective commands. the
+		// function here should not take any arguments.They have to ask for
+		// necessary arguments themselves. They shall return a exit status as [int]
+		// helper function will be static and flexible according to the needs
+	public:
+		// provide the starting path to the shell
+		fash(std::string path) {
+			this->current_path = path;
+			std::cout << "...initializing fash at - " << path << std::endl;
+			system("pause");
+			system("cls");
+		};
+		int welcome_msg(void);
+		int exit_msg(void);
+		int echo(void);
+		int run_sh(void);
+		int list_dir(void);
+		int change_dir(void);
+		int make_dir(void);
+		int remove_dir(void);
+		int remove_item(void);
+		int copy_item(void);
+		int clear_scr(void);
 
-    static bool path_exists(const std::string& path);
-    static void normalize_slashes(std::string& path);
-    static std::string minimize_path(const std::string& path);
+		static bool path_exists(const std::string& path);
+		static void normalize_slashes(std::string& path);
+		static std::string minimize_path(const std::string& path);
 };
